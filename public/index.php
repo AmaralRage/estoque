85f6555a4573e17178839c2c9331f5019c3893fa
The Amaral <82917923+AmaralRage@users.noreply.github.com>
2024-03-02 13:07:21 -0300

    testando

--- public/index.php
+++ public/index.php
@@ -20,8 +20,6 @@ if (file_exists($maintenance = __DIR__.'/../storage/framework/maintenance.php'))
     require $maintenance;
 }
 
-// testando estoque
-
 /*
 |--------------------------------------------------------------------------
 | Register The Auto Loader
